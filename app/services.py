a9285449eb1ae36a96f27daec671604c39bee2ae
SraReaper <carolin.cosgirl@gmail.com>
2026-09-22 14:02:51 -0300

    implement task update

--- app/services.py
+++ app/services.py
@@ -1,5 +1,6 @@
 from app.models import Task
 from app.storage import add_task, get_all_tasks as storage_get_all_tasks
+from app.storage import get_task_by_id
 
 next_id = 1
 
@@ -15,6 +16,14 @@ def create_task(title):
 
 def get_all_tasks():
     return storage_get_all_tasks()
+
+def update_title_task(task_id, new_title):
+    task = get_task_by_id(task_id)
+    if task:
+        task.title = new_title
+        return task
+    
+    return None
 
 
 
--- app/storage.py
+++ app/storage.py
@@ -6,3 +6,9 @@ def add_task(task):
 
 def get_all_tasks():
     return tasks
+
+def get_task_by_id(task_id):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    return None
