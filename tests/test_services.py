228985e1dc6e2d03f2c81136116abd8af8aff56b
SraReaper <carolin.cosgirl@gmail.com>
2026-09-22 22:37:29 -0300

    add test for empty task title

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,4 +1,5 @@
 from app.services import create_task
+from app.services import update_title_task
 
 
 def test_create_task():
@@ -7,4 +8,9 @@ def test_create_task():
     assert task.id == 1
     assert task.title == "Estudar Python"
     assert task.completed is False
+    
+def test_update_title_task_not_found():
+    result = update_title_task(56, "Estudar Java")
+
+    assert result is None
     
